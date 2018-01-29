8c751a05e951b20a9d74fdf87249f828b5651484
KenseiYo <yo.kensei@isid.co.jp>
2018-01-29 14:00:24 +0900

    ステータスの変更を開始と終了の両方設定出来るように変更

--- status-bot.py
+++ status-bot.py
@@ -5,6 +5,7 @@ import time
 import json
 from ConfigParser import SafeConfigParser, MissingSectionHeaderError
 
+from time import sleep
 from slackclient import SlackClient
 from logbook import Logger
 from logbook import RotatingFileHandler
@@ -17,8 +18,8 @@ logger = Logger("status-bot")
 def is_status_changed_event(res):
     return res["type"] == "user_change"
 
-def is_target_status_emoji(profile):
-    return profile["status_emoji"] == ":" + target_status + ":"
+def is_target_status_emoji(profile, status):
+    return profile["status_emoji"] == ":" + status + ":"
 
 def setup_logger(config):
     if config.has_option(SLACK_SECTION_NAME, "log_output"):
@@ -59,8 +60,10 @@ config = SafeConfigParser()
 read_config(config)
 token = parse_required_option(config, "token")
 room = parse_required_option(config, "room")
-target_status = parse_required_option(config, "status")
-message_format_on = parse_required_option(config, "message_format_on")
+status_start = parse_required_option(config, "status_start")
+status_end = parse_required_option(config, "status_end")
+message_format_start = parse_required_option(config, "message_format_start")
+message_format_end = parse_required_option(config, "message_format_end")
 setup_logger(config)
 sc = SlackClient(token)
 
@@ -76,9 +79,15 @@ if sc.rtm_connect():
                     user = res["user"]
                     profile = user["profile"]
                     name = user["name"]
-                    if is_target_status_emoji(profile):
-                        text = message_format_on.format(name)
+                    if is_target_status_emoji(profile, status_start):
+                        text = message_format_start.format(name)
                         sc.api_call("chat.postMessage", channel=room, text=text, as_user="1")
+                    if is_target_status_emoji(profile, status_end):
+                        text = message_format_end.format(name)
+                        sc.api_call("chat.postMessage", channel=room, text=text, as_user="1")
+                        sleep(5)
+                        profile["status_emoji"] = ""
+                        sc.api_call("users.profile.set", profile=profile)
 
 else:
     logger.critical("Connection Failed, invalid token?")
