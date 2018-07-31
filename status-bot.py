324f7532ca382e15b2a62665322344da16b70a91
yokensei <yo.kensei@isid.co.jp>
2018-07-31 21:21:37 +0900

    プロファイル更新時のユーザー指定を追加

--- status-bot.py
+++ status-bot.py
@@ -14,7 +14,28 @@ from logbook import StreamHandler
 SLACK_SECTION_NAME = "slack"
 logger = Logger("status-bot")
 
+def is_status_changed_event(res):
+    return res["type"] == "user_change"
 
+def is_target_status_emoji(profile, status):
+    return profile["status_emoji"] == ":" + status + ":"
+
+def setup_logger(config):
+"status-bot.py" 98L, 3578C
+# coding:utf-8
+import os
+import sys
+import time
+import json
+from ConfigParser import SafeConfigParser, MissingSectionHeaderError
+
+from time import sleep
+from slackclient import SlackClient
+from logbook import Logger
+from logbook import RotatingFileHandler
+from logbook import StreamHandler
+
+SLACK_SECTION_NAME = "slack"
 def is_status_changed_event(res):
     return res["type"] == "user_change"
 
@@ -28,15 +49,12 @@ def setup_logger(config):
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        file_handler = RotatingFileHandler(output_path, backup_count=5)
-        file_handler.push_application()
     else:
         stream_handler = StreamHandler(sys.stdout)
         stream_handler.push_application()
 
 
 def read_config(config):
-    error_message = "Please create '{}' section and contain options.".format(SLACK_SECTION_NAME)
     try:
         config.read("config.ini")
     except MissingSectionHeaderError:
@@ -50,7 +68,6 @@ def read_config(config):
 
 def parse_required_option(config, option):
     if not config.has_option(SLACK_SECTION_NAME, option):
-        print "Please setting '{}' option in '{}' section.".format(option, SLACK_SECTION_NAME)
         sys.exit()
 
     return config.get(SLACK_SECTION_NAME, option)
@@ -58,23 +75,142 @@ def parse_required_option(config, option):
 
 config = SafeConfigParser()
 read_config(config)
-slack_token = parse_required_option(config, "slack_token")
+token = parse_required_option(config, "slack_token")
 bot_token = parse_required_option(config, "bot_token")
-room = parse_required_option(config, "room")
-status_start = parse_required_option(config, "status_start")
 status_end = parse_required_option(config, "status_end")
 message_format_start = parse_required_option(config, "message_format_start")
-message_format_end = parse_required_option(config, "message_format_end")
-setup_logger(config)
-sc = SlackClient(slack_token)
+sc = SlackClient(token)
 bot_sc = SlackClient(bot_token)
 
-if sc.rtm_connect():
+if bot_sc.rtm_connect():
     logger.info("status-bot is up")
 
-    while True:
-        response = sc.rtm_read()
         for res in response:
+            logger.info(res)
+            if "type" in res:
+                if is_status_changed_event(res):
+                    user = res["user"]
+from logbook import StreamHandler
+
+SLACK_SECTION_NAME = "slack"
+def is_status_changed_event(res):
+    return res["type"] == "user_change"
+
+def is_target_status_emoji(profile, status):
+    return profile["status_emoji"] == ":" + status + ":"
+
+def setup_logger(config):
+    if config.has_option(SLACK_SECTION_NAME, "log_output"):
+        output_path = config.get(SLACK_SECTION_NAME, "log_output")
+        dir_path, file_name = os.path.split(output_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+    else:
+        stream_handler = StreamHandler(sys.stdout)
+        stream_handler.push_application()
+
+
+def read_config(config):
+    try:
+        config.read("config.ini")
+    except MissingSectionHeaderError:
+        print error_message
+        sys.exit()
+
+    if not config.has_section(SLACK_SECTION_NAME):
+        print error_message
+        sys.exit()
+
+
+def parse_required_option(config, option):
+    if not config.has_option(SLACK_SECTION_NAME, option):
+        sys.exit()
+
+    return config.get(SLACK_SECTION_NAME, option)
+
+
+config = SafeConfigParser()
+read_config(config)
+token = parse_required_option(config, "slack_token")
+bot_token = parse_required_option(config, "bot_token")
+status_end = parse_required_option(config, "status_end")
+message_format_start = parse_required_option(config, "message_format_start")
+sc = SlackClient(token)
+bot_sc = SlackClient(bot_token)
+
+if bot_sc.rtm_connect():
+    logger.info("status-bot is up")
+        response = bot_sc.rtm_read()
+            if "type" in res:
+                if is_status_changed_event(res):
+                    user = res["user"]
+# coding:utf-8
+import os
+import sys
+import time
+import json
+from ConfigParser import SafeConfigParser, MissingSectionHeaderError
+
+from time import sleep
+from slackclient import SlackClient
+from logbook import Logger
+from logbook import RotatingFileHandler
+from logbook import StreamHandler
+
+SLACK_SECTION_NAME = "slack"
+def is_status_changed_event(res):
+    return res["type"] == "user_change"
+
+def is_target_status_emoji(profile, status):
+    return profile["status_emoji"] == ":" + status + ":"
+
+def setup_logger(config):
+    if config.has_option(SLACK_SECTION_NAME, "log_output"):
+        output_path = config.get(SLACK_SECTION_NAME, "log_output")
+        dir_path, file_name = os.path.split(output_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+    else:
+        stream_handler = StreamHandler(sys.stdout)
+        stream_handler.push_application()
+
+
+def read_config(config):
+    try:
+        config.read("config.ini")
+    except MissingSectionHeaderError:
+        print error_message
+        sys.exit()
+
+    if not config.has_section(SLACK_SECTION_NAME):
+        print error_message
+        sys.exit()
+
+
+def parse_required_option(config, option):
+    if not config.has_option(SLACK_SECTION_NAME, option):
+        sys.exit()
+
+    return config.get(SLACK_SECTION_NAME, option)
+
+
+config = SafeConfigParser()
+read_config(config)
+token = parse_required_option(config, "slack_token")
+bot_token = parse_required_option(config, "bot_token")
+status_end = parse_required_option(config, "status_end")
+message_format_start = parse_required_option(config, "message_format_start")
+sc = SlackClient(token)
+bot_sc = SlackClient(bot_token)
+
+if bot_sc.rtm_connect():
+    logger.info("status-bot is up")
+        response = bot_sc.rtm_read()
+            if "type" in res:
+                if is_status_changed_event(res):
+                    user = res["user"]
             logger.info(res)
             if "type" in res:
                 if is_status_changed_event(res):
@@ -87,8 +223,13 @@ if sc.rtm_connect():
                     if is_target_status_emoji(profile, status_end):
                         text = message_format_end.format(name)
                         bot_sc.api_call("chat.postMessage", channel=room, text=text, as_user="1")
-                        new_profile = {"status_text":"","status_emoji":""}
-                        sc.api_call("users.profile.set", profile=new_profile)
+                        users = json.loads(sc.api_call("users.list"))
+                        if users["ok"]:
+                            for member in users["members"]:
+                                if member["name"] == name:
+                                    memberid = member["id"]
 
+                            new_profile = {"status_text":"","status_emoji":""}
+                            sc.api_call("users.profile.set", user=memberid, profile=new_profile, pretty="1")
 else:
     logger.critical("Connection Failed, invalid token?")
